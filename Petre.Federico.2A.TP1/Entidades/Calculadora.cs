d22d34f9115dbe7f053a20478e61a34b610a4733
FedericoPetre <federicopetre22@gmail.com>
2022-04-11 17:16:41 -0300

    primer commit tp1 labo 2

---
+++ Petre.Federico.2A.TP1/Entidades/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public double Operar(Operando num1, Operando num2, char operador)
+        {
+            double resultado=0;
+            char operandoValidado = ValidarOperador(operador);
+
+            switch (operandoValidado)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break; 
+                case '/':
+                    resultado = num1 / num2;
+                    break;
+            }
+
+            return resultado;
+        }
+
+        private static char ValidarOperador(char operador)
+        {
+            char operadorDeRetorno = '+';
+
+            switch (operador)
+            {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                operadorDeRetorno = operador;
+                    break;            
+            }
+
+            return operadorDeRetorno;        
+        }
+
+
+
+    }
+}
---
+++ Petre.Federico.2A.TP1/Entidades/Operando.cs
@@ -0,0 +1,186 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        /// <summary>
+        ///  Convierte a decimal, si es posible, el string binario ingresado por parámetro. Caso contrario retorna "Valor invalido"
+        /// </summary>
+        /// <param name="binario"></param>
+        /// <returns></returns>
+        public string BinarioDecimal(string binario) // se puede convertir -1000 en decimal?
+        {
+            string stringRetorno = "Valor invalido";
+            double numeroDecimal = 0;
+            int longitudBinario = binario.Length;
+
+            if (EsBinario(binario))
+            {
+                foreach (char caracter in binario)
+                {
+                    longitudBinario--;
+                    if (caracter == '1')
+                    {
+                        numeroDecimal = numeroDecimal + Math.Pow(2, longitudBinario);
+                    }
+                }
+
+                stringRetorno = numeroDecimal.ToString();                
+            }
+
+            return stringRetorno;
+        }
+
+        /// <summary>
+        /// Convierte a binario, si es posible, el valor absoluto del numero ingresado por parametro. 
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        public string DecimalBinario(double numero)
+        {
+            int potenciaMaximaDeDos = 0;
+            double numeroAConvertir = Math.Abs(numero);
+            string stringBinario = "";
+            double coeficiente;
+            char digitoAConcatenar;
+
+            while (numeroAConvertir >= Math.Pow(2, potenciaMaximaDeDos))
+            {
+                potenciaMaximaDeDos++;
+            }
+
+            for (int i = 0; i < potenciaMaximaDeDos; i++)
+            {
+                if (numeroAConvertir % 2 == 0)
+                {
+                    coeficiente = numeroAConvertir / 2;
+                    digitoAConcatenar = '0';
+                }
+                else
+                {
+                    coeficiente = (numeroAConvertir - 1) / 2;
+                    digitoAConcatenar = '1';
+                }
+
+                stringBinario = digitoAConcatenar + stringBinario;
+                numeroAConvertir = coeficiente;
+            }
+
+            return stringBinario;
+
+        }
+
+        /// <summary>
+        /// Convierte a binario, si es posible, el valor absoluto del numero string ingresado por parametro. Caso contrario retorna "Valor invalido"
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        public string DecimalBinario(string numero)
+        {
+            string stringRetorno = "Valor invalido";
+            double doubleNumero;
+            bool boolConversor = double.TryParse(numero, out doubleNumero);
+
+            if (boolConversor)
+            {
+                stringRetorno = DecimalBinario(doubleNumero);            
+            }
+
+            return stringRetorno;
+        
+        }
+
+        /// <summary>
+        /// Determina si un string número está compuesto sólo por 0 y 1. (Es binario)
+        /// </summary>
+        /// <param name="binario"></param>
+        /// <returns></returns>
+        private bool EsBinario(string binario)
+        {
+            bool flagEsBinario = true;
+
+            foreach (char caracter in binario)
+            {
+                if (caracter != '1' && caracter != '0')
+                {
+                    flagEsBinario = false;
+                    break;
+                }
+            }
+
+            return flagEsBinario;        
+        }
+
+        public Operando()
+        {
+            this.numero = 0;
+        }
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero) : this(double.Parse(strNumero))
+        {
+            
+        }
+
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Operando n1, Operando n2)
+        {
+            double resultado;
+
+            if (n2.numero == 0)
+            {
+                resultado = double.MinValue;
+            }
+            else
+            { 
+                resultado = n1.numero / n2.numero;
+            }
+
+            return resultado;
+        }
+
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        private static double ValidarOperando(string strNumero)
+        {
+            double numeroConvertido;
+            bool flagConvertido = double.TryParse(strNumero, out numeroConvertido);
+
+            if (!flagConvertido)
+            {
+                numeroConvertido = 0;
+            }
+
+            return numeroConvertido;
+        }
+
+
+    }
+}
